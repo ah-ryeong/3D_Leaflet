c2f62e68e37278b146240d205a8b5709c655f789
ah-ryeong <aryeong22@hanmail.net>
2021-06-10 22:34:25 +0900

    close 버튼 활성화

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (() => {
     // 익명함수 직접호출
     const leaflet = document.querySelector('.leaflet');
+    let pageCount = 0;
 
     function getTarget(elem, className) {
         while(!elem.classList.contains(className)) {
@@ -21,6 +22,11 @@
 
         if (pageElem) {
             pageElem.classList.add('page-flipped');
+            pageCount ++;
+            
+            if (pageCount == 2) {
+                document.body.classList.add('leaflet-opened');
+            }
         }
     }); 
 })();
