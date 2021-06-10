bf00f27a19bbb508bfc2acd7b9f98dc56984bf78
ah-ryeong <aryeong22@hanmail.net>
2021-06-10 22:43:25 +0900

    close 버튼 동작완료

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (() => {
     // 익명함수 직접호출
     const leaflet = document.querySelector('.leaflet');
+    const pageElems = document.querySelectorAll('.page');
     let pageCount = 0;
 
     function getTarget(elem, className) {
@@ -15,6 +16,16 @@
         return elem;
     }
 
+    function closeLeaflet() {
+        pageCount = 0;
+        document.body.classList.remove('leaflet-opened');
+        pageElems[2].classList.remove('page-flipped');
+        // index 2를 닫고 시간을 두고 같이 닫아줌
+         setTimeout(() => {
+            pageElems[0].classList.remove('page-flipped');
+        }, 500);
+    }
+
     leaflet.addEventListener('click', e =>{
         // console.log(e.target);
         let pageElem = getTarget(e.target, 'page')
@@ -27,6 +38,13 @@
             if (pageCount == 2) {
                 document.body.classList.add('leaflet-opened');
             }
+        }
+
+        // close 버튼 누르면 동작
+        let closeBtnElem = getTarget(e.target, 'close-btn');
+        
+        if (closeBtnElem) {
+            closeLeaflet();
         }
     }); 
 })();
